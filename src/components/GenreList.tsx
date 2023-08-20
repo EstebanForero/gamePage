08daee0144a4faa2792e8676178f6c4c8a2c9276
Esteban Forero <estebanfomo@unisabana.edu.co>
2023-08-20 17:11:38 -0500

    improve genres display

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,4 +1,4 @@
-import { Button, HStack, Image, List, ListItem, Spinner } from "@chakra-ui/react"
+import { Button, HStack, Heading, Image, List, ListItem, Spinner } from "@chakra-ui/react"
 import useGenres, { Genre } from "../hooks/useGenres"
 import getCroppedImageUrl from "../services/Image-url"
 
@@ -14,14 +14,17 @@ const GenreList = ( { selectedGenre, onSelectGenre } : Props ) => {
   if (error) return null
 
   return (
+    <>
+    <Heading fontSize='2xl' marginBottom={3}>Genres</Heading>
     <List>
         {data.map(genre => <ListItem key={genre.id} paddingY='5px'>
           <HStack>
-          <Image src={getCroppedImageUrl(genre.image_background)} alt={genre.name} boxSize='32px' borderRadius={8}/>
-          <Button fontWeight={selectedGenre?.id === genre.id ? 'bold' : 'normal' } onClick={ () =>  onSelectGenre(genre)} fontSize='lg' variant='link'>{genre.name}</Button>
+          <Image objectFit='cover' src={getCroppedImageUrl(genre.image_background)} alt={genre.name} boxSize='32px' borderRadius={8}/>
+          <Button whiteSpace='normal' textAlign='left' fontWeight={selectedGenre?.id === genre.id ? 'bold' : 'normal' } onClick={ () =>  onSelectGenre(genre)} fontSize='lg' variant='link'>{genre.name}</Button>
           </HStack>
         </ListItem>)}
     </List>
+    </>
   )
 }
 
