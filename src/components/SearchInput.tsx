a036f2877876457680509abf8b0786b2d29fa68a
Esteban Forero <estebanfomo@unisabana.edu.co>
2023-08-20 18:18:57 -0500

    UpdatePage title

--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -8,7 +8,7 @@ interface Props {
 
 const SearchInput = ({onSearch}: Props) => {
 
-    const ref = useRef<HTMLInputElement>()
+    const ref = useRef<HTMLInputElement>(null)
 
   return (
     <form onSubmit={(event) => {
